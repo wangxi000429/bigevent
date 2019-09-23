215299d66a9be50f671d2228de866e6d3541b524
unknown <1006929517@qq.com>
2019-09-23 21:28:46 +0800

    add

--- web_back/js/utils/category.js
+++ web_back/js/utils/category.js
@@ -4,7 +4,12 @@ var category = {
     show : function(callBack) {
         $.get(URLIST.category_show , function(res) {
             callBack(res);
-        })
+        });
+    },
+    add : function(name,slug,callBack) {
+        $.post(URLIST.category_add,{name:name,slug:slug},function(res) {
+            callBack(res);
+        });
     }
 
 }
--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -3,7 +3,9 @@ var URLIST = {
     user_login : BASE_URL + '/admin/login',
     user_logout : BASE_URL + '/admin/logout',
     user_info : BASE_URL + '/admin/getuser',
-    category_show : BASE_URL + '/admin/category_search'
+    category_show : BASE_URL + '/admin/category_search',
+    category_add : BASE_URL + '/admin/category_add',
+
 };
 
 
