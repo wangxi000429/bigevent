3cb939778eec4ce482709ef881291d8a9436b226
unknown <1006929517@qq.com>
2019-09-24 16:14:23 +0800

    编辑功能完成

--- web_back/js/utils/category.js
+++ web_back/js/utils/category.js
@@ -1,20 +1,46 @@
-
 //文章类别
 var category = {
-    show : function(callBack) {
-        $.get(URLIST.category_show , function(res) {
+    show: function (callBack) {
+        $.get(URLIST.category_show, function (res) {
             callBack(res);
         });
     },
-    add : function(name,slug,callBack) {
-        $.post(URLIST.category_add,{name:name,slug:slug},function(res) {
+    /**
+     * 
+     * @param {*} name 名称
+     * @param {*} slug 别名
+     * @param {*} callBack 回调
+     */
+    add: function (name, slug, callBack) {
+        $.post(URLIST.category_add, {
+            name: name,
+            slug: slug
+        }, function (res) {
             callBack(res);
         });
-    }, 
-    del: function(id, callBack) {
-        $.post(URLIST.category_del, { id: id }, function(res) {
-          callBack(res);
+    },
+    del: function (id, callBack) {
+        $.post(URLIST.category_del, {
+            id: id
+        }, function (res) {
+            callBack(res);
         });
-      }
+    },
+    /**
+     * 
+     * @param {*} id  
+     * @param {*} name 名称
+     * @param {*} slug 别名
+     * @param {*} callBack 回调
+     */
+    edit: function (id, name, slug, callBack) {
+        $.post(URLIST.category_edit, {
+            id: id,
+            name: name,
+            slug: slug
+        }, function (res) {
+            callBack(res);
+        });
+    }
 
 }
--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -6,6 +6,7 @@ var URLIST = {
     category_show : BASE_URL + '/admin/category_search',
     category_add : BASE_URL + '/admin/category_add',
     category_del: BASE_URL + '/admin/category_delete',
+    category_edit: BASE_URL + '/admin/category_edit'
 
 };
 
