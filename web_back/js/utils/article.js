eb465af7f7f7a73b844257167e8bea89c74ad232
unknown <1006929517@qq.com>
2019-09-24 20:30:01 +0800

    文章分页显示和筛选功能完成

---
+++ web_back/js/utils/article.js
@@ -0,0 +1,12 @@
+var article = {
+    /**
+     * 
+     * @param {*} param  {key:'abc', type:1,state:'草稿'}
+     * @param {*} callBack 
+     */
+    show : function(param,callBack) {
+        $.get(URLIST.article_show, param, function(res) {
+            callBack(res);
+          });
+    },
+}
--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -3,10 +3,17 @@ var URLIST = {
     user_login : BASE_URL + '/admin/login',
     user_logout : BASE_URL + '/admin/logout',
     user_info : BASE_URL + '/admin/getuser',
+
+
     category_show : BASE_URL + '/admin/category_search',
     category_add : BASE_URL + '/admin/category_add',
     category_del: BASE_URL + '/admin/category_delete',
-    category_edit: BASE_URL + '/admin/category_edit'
+    category_edit: BASE_URL + '/admin/category_edit',
+
+
+    article_show: BASE_URL + '/admin/search',
+
+
 
 };
 
